2f7216eef1b616dcbca9a5807334d29db3503a41
SID <stiwari@tcd.ie>
2019-10-11 13:42:20 +0100

    Updates

---
+++ precache-manifest.62ed9c3d7de0cef0523cdb2e5d70dc21.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "cc51f3a8f3dfde3cb8b8",
+    "url": "/static/js/main.cc51f3a8.chunk.js"
+  },
+  {
+    "revision": "6eb6405c9d99258624bc",
+    "url": "/static/js/1.6eb6405c.chunk.js"
+  },
+  {
+    "revision": "cc51f3a8f3dfde3cb8b8",
+    "url": "/static/css/main.6a77f7bc.chunk.css"
+  },
+  {
+    "revision": "5171505f6ac522ed171d0d99a9f0c2d3",
+    "url": "/index.html"
+  }
+];
