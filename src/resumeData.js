0cf6078d216012011e3e9fe9e9b7430fc067ae3b
SID <stiwari@tcd.ie>
2019-10-11 12:27:42 +0100

    Code ++

---
+++ src/resumeData.js
@@ -0,0 +1,109 @@
+let resumeData = {
+    "imagebaseurl":"https://rbhatia46.github.io/",
+    "name": "Siddharth Tiwari",
+    "role": "Frontend Developer and Data Scientist",
+    "linkedinId":"Your LinkedIn Id",
+    "skypeid": "Your skypeid",
+    "roleDescription": " I like dabbling in various parts of frontend development and like to learn about new technologies, write technical articles or simply play games in my free time.",
+    "socialLinks":[
+        {
+          "name":"linkedin",
+          "url":"https://www.linkedin.com/in/sidtwr/",
+          "className":"fa fa-linkedin"
+        },
+        {
+          "name":"github",
+          "url":"https://github.com/SIDtwr",
+          "className":"fa fa-github"
+        },
+        {
+          "name":"twitter",
+          "url":"https://twitter.com/sid25496389",
+          "className":"fa fa-twitter"
+        }
+      ],
+    "aboutme":"I am final year student MSc. Computer Science Student at Trinity College Dublin. I am a self taught Full Stack Web Developer, currently diving deeper into Machine Learning. I believe to be successful in life, one needs to be believe in their dreams and keep working towards them. I like to describe myself as an idealist without illusions \" quoting JFK \".",
+    "address":"Ireland",
+    "website":"stiwari@tcd.ie",
+    "education":[
+      {
+        "UniversityName":"Trinity College Dublin",
+        "specialization":"MSc. Computer Science Intelligent Systems",
+        "MonthOfPassing":"Sep",
+        "YearOfPassing":"2019",
+        "Achievements":"TCD's M.Sc. Computer Science changed my entire understanding of learning. The course equipped me to apply my technical competencies to theoretical pedagogy and real-world practice, as well as challenging me with the rigor of academic research. The best aspect of the course was definitely the friendly, accessible approach of the instructors and the life-long friendships made with classmates."
+      },
+      {
+        "UniversityName":"Guru Gobind Singh Indraprastha University",
+        "specialization":"B.Tech Computer Science & Engineering",
+        "MonthOfPassing":"May",
+        "YearOfPassing":"2017",
+        "Achievements":"My undergraduate course helped me build a strong foundation in computer science, with strong competencies in algorithms, data structures, and programming languages."
+      }
+    ],
+    "work":[
+      {
+        "CompanyName":"Busy Infotech",
+        "specialization":"Junior Software Developer",
+        "MonthOfLeaving":"Aug",
+        "YearOfLeaving":"2018",
+        "Achievements":"I have eight months of professional work experience as a Junior Software Developer in the R&D department of Busy Infotech. During my tenure, I designed user interactive web pages as the Front-End part of the web application using various web technologies like HTML, CSS, JavaScript, jQuery, AJAX and used Bootstrap framework for a more consistent appearance and feel. Took agile software development initiatives and review using pull requests and achieved superior code quality which resulted in 12% decrease in bugs, code smells and vulnerabilities in the codebase. Performed Gap Analysis for their online portal and recommended improvements for the same."
+      },
+      {
+        "CompanyName":"Indian Navy",
+        "specialization":"Internship",
+        "MonthOfLeaving":"Aug",
+        "YearOfLeaving":"2016",
+        "Achievements":"During my internship at WESEE, I worked on ARM STM32Fxx series micro-controllers and programmed it in C / C++ to function as a SHA-2 encryption device that ciphered and deciphered data that was transferred from the PC to the SD card plugged into the microcontroller. Also, I learned fundamental of cyber security such as Advanced Encryption Standards (AES), Security Architecture Design and networking protocols such as TCP/IP, FTP etc."
+      }
+    ],
+    "skillsDescription":"Over the course of my education I have acquired the following skills",
+    "skills":[
+      {
+        "skillname":"HTML5"
+      },
+      {
+        "skillname":"CSS"
+      },
+      {
+        "skillname":"JavaScript"
+      },
+      {
+        "skillname":"Reactjs"
+      }
+    ],
+    "portfolio":[
+      {
+        "name":"Travel Companian Finder Mobile App",
+        "description":"mobileapp",
+        "imgurl":"images/portfolio/Companion.jpg"
+      },
+      {
+        "name":"Twitter Gender Predicition - Machine Learning ",
+        "description":"Machine Learning",
+        "imgurl":"images/portfolio/project.png"
+      },
+      {
+        "name":"Video Game Industry Analysis - Data Visulization",
+        "description":"Data Viz",  
+        "imgurl":"images/portfolio/project2.png"
+      },
+      {
+        "name":"Search Engine Usinng Apache Lucene",
+        "description":"Search Engine",
+        "imgurl":"images/portfolio/search.jpg"
+      }
+    ],
+    "testimonials":[
+      {
+        "description":"This is a sample testimonial",
+        "name":"Some technical guy"
+      },
+      {
+        "description":"This is a sample testimonial",
+        "name":"Some technical guy"
+      }
+    ]
+  }
+  
+  export default resumeData
